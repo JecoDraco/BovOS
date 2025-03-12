f17a9df784fdc70d756461e629fc5e4795a69b51
AGM0311 <axlgamas@outlook.com>
2025-03-11 20:42:32 -0600

    Descripción de los cambios realizados

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 export function Home() {
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1 style={{ color: 'blue' }}>¡Bienvenido a la Página de Inicio!</h1>
-      <p>Esta es una página de prueba para el componente Home.</p>
-      <p>Puedes agregar más contenido aquí, como imágenes, botones, o enlaces.</p>
+    <div>
     </div>
   );
 }
